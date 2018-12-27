08a26513860bc8ab47c8841f85928bd8b2dc131c
delalande <tdelalande@protonmail.com>
2018-12-27 18:16:44 +0800

    change to social network

---
+++ playground/src/components/Post.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Post extends React.Component 
+    {    
+        render() {
+            return (
+                <div>
+                    <h3>Title of Post</h3>
+                    <p>Contents of Post</p>
+                </div>
+            )
+    }
+}
+
+export default Post
