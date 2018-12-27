5446eb1a199f3dbfffb1f874fba0b52674ad891e
delalande <tdelalande@protonmail.com>
2018-12-27 18:41:20 +0800

    uploaded postsdata

---
+++ playground/src/components/PostsData.js
@@ -0,0 +1,34 @@
+const postsData = [
+    {
+        id:1,
+        title:'Post 1',
+        text:'Text of Post1'
+    },
+    {
+        id:2,
+        title:'Post 2',
+        text: 'Text of Post 2'
+    },
+    {
+        id:3,
+        title:'Post 3',
+        text:'text of Post 3'
+    },
+    {
+        id:4,
+        title: 'Post 4',
+        text: ' text of post 4'
+    },
+    {
+        id:5,
+        title:'post 5',
+        text:'text of post 5'
+    },
+    {
+        id:6,
+        title: 'post 6',
+        text: 'tedt of post 6'
+    }
+]
+
+export default postsData
